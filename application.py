ceb7fad4280dd87e8c435f4d4b55034c0914995d
Shreya Sahu <shsah@microsoft.com>
2019-07-24 01:07:32 +0530

    working till now, entries getting updated in unique problems tab;e

--- application.py
+++ application.py
@@ -1,7 +1,70 @@
 from flask import Flask
-from flask import json, jsonify
+from flask import json, jsonify, request
+import ssl
+import pymongo
+from pymongo import MongoClient
+from difflib import SequenceMatcher
 app = Flask(__name__)
+
+uri = "mongodb://hackathon2019:dp0O0VMaTwXU8PUwdYUxEgeu4UNOeVB5Ve7kU5zqV3tmfz3gNb1sc6VMY3HCjUcBC4Ubq3T9fXs6lJfMUfVIXg==@hackathon2019.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+client = pymongo.MongoClient(uri)
+
+# Get the database name
+db = client.EmpowerMS
+db.authenticate(name="hackathon2019",password="dp0O0VMaTwXU8PUwdYUxEgeu4UNOeVB5Ve7kU5zqV3tmfz3gNb1sc6VMY3HCjUcBC4Ubq3T9fXs6lJfMUfVIXg==")
 
 @app.route("/", methods = ['POST'])
 def api_message():
-   return jsonify({"data": "yay"})
+
+    # Get the collection
+    uniqueProblems = db.UniqueProblems
+
+    # Get the new problem from post data
+    newProblem = request.json
+
+    maxSimilarityScore = 0
+    maxSimilarProblemId = None
+
+    # Iterating through every unique problem of the same tag and calculating the similarity score with each. 
+    for uniqueProblem in uniqueProblems.find({"tag" : newProblem["tag"]}):
+        similarityScore = similar(newProblem["problemStatement"], uniqueProblem["problemStatement"])
+        if similarityScore > maxSimilarityScore:
+            maxSimilarityScore = similarityScore
+            maxSimilarProblemId = uniqueProblem["_id"]
+
+    newUniqueProblemEntry = {
+            "title" : newProblem["title"],
+            "problemStatement" : newProblem["problemStatement"],
+            "tag" : newProblem["tag"],
+            "contactAsker" : newProblem["contactAsker"],
+            "relatedProblems" : {},
+            "solutionId" : ""
+        }
+
+    if maxSimilarityScore >= 0.8:
+        # Do something
+        uniqueProblem = uniqueProblems.find_one({"_id" : maxSimilarProblemId})
+
+        updatedList = uniqueProblem["relatedProblems"]
+        updatedList.append(newUniqueProblemEntry)
+
+        uniqueProblems.update(
+            {"_id" : maxSimilarProblemId},
+            {
+                "$set": {
+                    "relatedProblems" : updatedList 
+                }
+            }
+        )
+        print ("updated the related doc")
+        
+    else:
+        # Treat this as a new problem, make a new entry in the UniqueProblems DB
+        uniqueProblems.insert(newUniqueProblemEntry)
+        print ("New entry inserted in Unique Problems table")
+
+    return "true"
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
