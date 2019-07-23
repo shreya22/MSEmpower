76ae961e0b183d56cd50b31164b89397768c8f58
Shreya Sahu <shsah@microsoft.com>
2019-07-23 20:34:36 +0530

    first commit

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask import json, jsonify
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/", methods = ['POST'])
+def api_message():
+   return jsonify({"data": "yay"})
